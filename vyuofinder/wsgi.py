212e2ab2e689270be81bedae12a6dcfc7fca287a
felician <felicianjoseph575@gmail.com>
2026-06-13 16:06:24 -0700

    Ensure admin account on startup

--- vyuofinder/wsgi.py
+++ vyuofinder/wsgi.py
@@ -9,8 +9,25 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vyuofinder.settings')
 
 application = get_wsgi_application()
+
+
+def ensure_default_admin():
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+    if not username or not password:
+        return
+
+    User = get_user_model()
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+
+
+ensure_default_admin()
