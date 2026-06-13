e0b986763a4fa21fc33c15dff8bf8664fbddbc3a
felician <felicianjoseph575@gmail.com>
2026-06-13 14:21:53 -0700

    fix page not found

--- universitysite/urls.py
+++ universitysite/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from universitysite import views
 app_name = 'universitysite'
 urlpatterns = [
-    path("home/", views.home, name = 'home'),
+    path(" ", views.home, name = 'home'),
     path('home/course/<int:pk>/', views.course_detail, name='course_detail'),
     path('home/university/<int:pk>/', views.univ_detail, name='universi'),
     path('home/<int:pk>/', views.cou_detail, name = 'cou'),
