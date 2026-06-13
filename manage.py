dc2c9084849974fcf2df78474986a91d3b51a1dd
felician <felicianjoseph575@gmail.com>
2026-06-13 14:51:52 -0700

    Add superuser creation script

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.contrib.auth.models import User
 
 
 def main():
@@ -15,8 +16,19 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    create_admin()    
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
     main()
+    
+# Hii ni njia ya haraka ya kuunda user wakati wa startup
+
+
+def create_admin():
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'password123')
+        print("Superuser created!")
+
+# Kisha kwenye main() yako, iite function hiyo:
