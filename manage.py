3ac6a1236bc600c83c7366450e46fbfa63b32c21
felician <felicianjoseph575@gmail.com>
2026-06-13 14:58:53 -0700

    fix user

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.contrib.auth.models import User
+
 
 
 def main():
@@ -16,7 +16,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    create_admin()    
+       
     execute_from_command_line(sys.argv)
 
 
@@ -26,9 +26,6 @@ if __name__ == '__main__':
 # Hii ni njia ya haraka ya kuunda user wakati wa startup
 
 
-def create_admin():
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'password123')
-        print("Superuser created!")
+
 
 # Kisha kwenye main() yako, iite function hiyo:
