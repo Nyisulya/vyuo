976c6bffe1431f5ec5b1fbced695afda27811f0e
felician <felicianjoseph575@gmail.com>
2026-06-13 14:28:26 -0700

    fix

--- vyuofinder/urls.py
+++ vyuofinder/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include('universitysite.urls')),
+   
     path('', include('universitysite.urls')),
     path('admin/', admin.site.urls),
 ]
