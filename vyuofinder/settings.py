a9f968b2f2e3ef595164154d97f36079f747ba7d
felician <felicianjoseph575@gmail.com>
2026-06-16 02:01:24 -0700

    change allowed host kwenye seting

--- vyuofinder/settings.py
+++ vyuofinder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-u$2nh8u+^(*_mf98hsl8v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
+ALLOWED_HOSTS = ['vyuotanzania.nyisu.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
