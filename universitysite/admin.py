fbda224d17468a0a8a102c782f37b0618279f665
felician <felicianjoseph575@gmail.com>
2026-06-23 00:41:00 -0700

    Added django-import-export for bulk data entry via Excel/CSV

--- universitysite/admin.py
+++ universitysite/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import University, Course, Region, UniversityCourse, Requirement
 
 # Register your models here.
 
+@admin.register(Region)
+class RegionAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(Region)
-admin.site.register(Course)
-admin.site.register(University)
-admin.site.register(UniversityCourse)
-admin.site.register(Requirement)
+@admin.register(Course)
+class CourseAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(University)
+class UniversityAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(UniversityCourse)
+class UniversityCourseAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Requirement)
+class RequirementAdmin(ImportExportModelAdmin):
+    pass
     
