6777a3df7669c5d7377c786cc94a616796b5a18e
felician <felicianjoseph575@gmail.com>
2026-06-28 12:31:28 -0700

    Fix STATICFILES_DIRS so images are found during collectstatic

--- vyuofinder/settings.py
+++ vyuofinder/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'universitysite', 'static'),
+]
 STORAGES = {
     'default': {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
