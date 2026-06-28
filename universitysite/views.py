5f09fac19fcac33859cf1ac91f27e8a57d468a23
felician <felicianjoseph575@gmail.com>
2026-06-28 13:21:09 -0700

    Import Feedback model in views.py

--- universitysite/views.py
+++ universitysite/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Region, University, Course, UniversityCourse
+from .models import Region, University, Course, UniversityCourse, Feedback
 from django.core.paginator import Paginator
 from django.contrib.admin.views.decorators import staff_member_required
 # Create your views here.
